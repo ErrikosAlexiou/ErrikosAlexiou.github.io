814db55c5e302997e2c17d820adab417163d8a30
Errikos Alexiou <errikos.alexiou@gmail.com>
2022-09-14 14:21:00 +0100

    initial commitial

---
+++ main.py
@@ -0,0 +1,14 @@
+import os
+import time
+import webbrowser
+
+webbrowser.open_new_tab(r"https://www.youtube.com/")
+time.sleep(0.01)
+webbrowser.open_new_tab(r"https://mail.google.com/mail/")
+time.sleep(0.01)
+webbrowser.open_new_tab(
+    r"https://www.evernote.com/shard/s613/nl/224040091/a65cb25f-47ae-430e-8022-8c9679507a3b?title=Todo%20List"
+)
+
+
+os.system("start index.html")
